1429304726fbe539244f6dd6752237f0597b1902
Erick Mulindi <37551785+mulindierick@users.noreply.github.com>
2021-11-01 17:40:40 +0300

    Intial commit

--- twitter-login.js
+++ twitter-login.js
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
 import puppeteer from "puppeteer";
 
 (async () => {
@@ -9,53 +11,40 @@ import puppeteer from "puppeteer";
     waitUntil: "networkidle0",
   });
 
-  // click on username input and type in username
+  // wait fot username input, click on username input and type in username
+  await page.waitForSelector("input[name=username]");
   await page.click("input[name=username]");
-  await page.type("input[name=username]", "mulindierick@gmail.com");
+  await page.type("input[name=username]", process.env.EMAIL);
 
-  // get the next button by classaname and click next
+  // get the next button by classaname and click it
   const btn = await page.$(".r-18jsvk2");
   await btn.evaluate((form) => form.click());
 
-  // click on username (text) input and type in username (text)
+  // wait for username(text) input to load, click on username (text) input and type in username (text)
   await page.waitForSelector("input[name=text]");
   await page.click("input[name=text]");
-  await page.type("input[name=text]", "mulindierick");
+  await page.type("input[name=text]", `${process.env.NAM}`);
 
+  //wait for the next button and click it 
   await page.waitForSelector(".r-18jsvk2");
   const btn1 = await page.$(".r-18jsvk2");
   await btn1.evaluate((form) => form.click());
 
-  // get the next button by classaname and click next
-  // const btn = await page.$(".r-18jsvk2");
-
-  // click on password input and type in the password
+  // wait for password input, click on password input and type in the password
   await page.waitForSelector("input[name=password]");
   await page.click("input[name=password]");
-  await page.type("input[name=password]", "mulindI12");
+  await page.type("input[name=password]", process.env.PASSWORD);
 
+  //wait for login button and login
   await page.waitForSelector(".r-18jsvk2");
   const btn2 = await page.$(".r-18jsvk2");
   await btn2.evaluate((form) => form.click());
 
-  await page.waitForNavigation()
-
-  // get the login button by classaname and login
-  // const btn = await page.$(".r-18jsvk2");
-  // await btn.evaluate((form) => form.click());
+  // wait for the page after login in
+  await page.waitForNavigation();
 
   //take screenshoot
   await page.screenshot({ path: "example.png" });
 
   await browser.close();
 })();
-
-// get the button by id and click on it
-
-// document.getElementById("gsi_294462_929240").style.backgroundColor = "red"
-
-// get the right gmail account
-
-// document.getElementsByClassName("fFW7wc-ibnC6b")[0].style.backgroundColor = "red"
-
-// https://mobile.twitter.com/i/flow/login
