c67f2adac9254cddd6afda6a580e2120e438d0a9
Erick Mulindi <37551785+mulindierick@users.noreply.github.com>
2021-11-01 18:28:41 +0300

    Refactored solution

--- twitter-login.js
+++ twitter-login.js
@@ -5,45 +5,28 @@ import puppeteer from "puppeteer";
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-
-  // go to url and wait for the page to load
   await page.goto("https://mobile.twitter.com/i/flow/login", {
     waitUntil: "networkidle0",
   });
 
-  // wait fot username input, click on username input and type in username
+  // wait for username input. type in username. click next btn
   await page.waitForSelector("input[name=username]");
-  await page.click("input[name=username]");
   await page.type("input[name=username]", process.env.EMAIL);
+  await page.$eval(".r-18jsvk2", (btn) => btn.click());
 
-  // get the next button by classaname and click it
-  const btn = await page.$(".r-18jsvk2");
-  await btn.evaluate((form) => form.click());
-
-  // wait for username(text) input to load, click on username (text) input and type in username (text)
+  // wait for username(text) input. type in username (text). click next btn
   await page.waitForSelector("input[name=text]");
-  await page.click("input[name=text]");
   await page.type("input[name=text]", `${process.env.NAM}`);
+  await page.$eval(".r-18jsvk2", (btn) => btn.click());
 
-  //wait for the next button and click it 
-  await page.waitForSelector(".r-18jsvk2");
-  const btn1 = await page.$(".r-18jsvk2");
-  await btn1.evaluate((form) => form.click());
-
-  // wait for password input, click on password input and type in the password
+  // wait for password input. type in the password. login
   await page.waitForSelector("input[name=password]");
-  await page.click("input[name=password]");
   await page.type("input[name=password]", process.env.PASSWORD);
-
-  //wait for login button and login
-  await page.waitForSelector(".r-18jsvk2");
-  const btn2 = await page.$(".r-18jsvk2");
-  await btn2.evaluate((form) => form.click());
+  await page.$eval(".r-18jsvk2", (btn) => btn.click());
 
   // wait for the page after login in
   await page.waitForNavigation();
 
-  //take screenshoot
   await page.screenshot({ path: "example.png" });
 
   await browser.close();
