782930359decca72bb8750c768b4b070d477b78d
Erick Mulindi <37551785+mulindierick@users.noreply.github.com>
2021-11-02 14:11:40 +0300

    improved first solution

--- twitter-login.js
+++ twitter-login.js
@@ -9,15 +9,21 @@ import puppeteer from "puppeteer";
     waitUntil: "networkidle0",
   });
 
+  // add condition to check if user name has been requested
+  // review repos
+  //create a new branch for the new changes
+
   // wait for username input. type in username. click next btn
   await page.waitForSelector("input[name=username]");
   await page.type("input[name=username]", process.env.EMAIL);
   await page.$eval(".r-18jsvk2", (btn) => btn.click());
 
   // wait for username(text) input. type in username (text). click next btn
-  await page.waitForSelector("input[name=text]");
-  await page.type("input[name=text]", `${process.env.NAM}`);
-  await page.$eval(".r-18jsvk2", (btn) => btn.click());
+  let requireUsername = await page.waitForSelector("input[name=text]");
+  if (requireUsername) {
+    await page.type("input[name=text]", `${process.env.NAM}`);
+    await page.$eval(".r-18jsvk2", (btn) => btn.click());
+  }
 
   // wait for password input. type in the password. login
   await page.waitForSelector("input[name=password]");
@@ -26,6 +32,7 @@ import puppeteer from "puppeteer";
 
   // wait for the page after login in
   await page.waitForNavigation();
+  await page.waitForTimeout(5000);
 
   await page.screenshot({ path: "example.png" });
 
